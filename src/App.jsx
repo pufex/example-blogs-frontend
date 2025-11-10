a67d0709cd143b3e9612534960c484bdaf3f1258
Jakub Abram <j.abram1@wp.pl>
2025-11-10 19:46:21 +0100

    Added simple route navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import { Routes, Route } from "react-router-dom"
 import { Blogs } from "./pages/Blogs"
 import { BlogForm } from "./pages/BlogForm"
+import { Link } from "react-router-dom"
 
 function App() {
   return <>
-    
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/create-blog">
+            New blog
+          </Link>
+        </li>
+      </ul>
+    </nav>
     <Routes>
       <Route path="/" element={<Blogs />} />
       <Route path="/create-blog" element={<BlogForm />}/>
