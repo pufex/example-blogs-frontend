9b0bef80b5639a68f81d5410598c9af2eb39da29
Jakub Abram <j.abram1@wp.pl>
2025-11-10 19:45:16 +0100

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom"
+import { Blogs } from "./pages/Blogs"
+import { BlogForm } from "./pages/BlogForm"
+
 function App() {
   return <>
-   
+    
+    <Routes>
+      <Route path="/" element={<Blogs />} />
+      <Route path="/create-blog" element={<BlogForm />}/>
+    </Routes>
   </>
+  
 }
 
 export default App
