2f96d559dce86f106c38bec67a26185dd5e45874
Jakub Abram <j.abram1@wp.pl>
2025-11-10 20:01:56 +0100

    Moved navbar to src/layouts/Navbar.jsx and made it a layout for / routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,17 @@
 import { Routes, Route } from "react-router-dom"
 import { Blogs } from "./pages/Blogs"
 import { BlogForm } from "./pages/BlogForm"
-import { Link } from "react-router-dom"
 import { SingleBlog } from "./pages/SingleBlog"
+import { Navbar } from "./layouts/Navbar"
 
 function App() {
   return <>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/create-blog">
-            New blog
-          </Link>
-        </li>
-      </ul>
-    </nav>
     <Routes>
-      <Route path="/" element={<Blogs />} />
-      <Route path="/create-blog" element={<BlogForm />}/>
-      <Route path="/:id" element={<SingleBlog />}/>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Blogs />} />
+        <Route path="create-blog" element={<BlogForm />}/>
+        <Route path=":id" element={<SingleBlog />}/>
+      </Route>
     </Routes>
   </>
   
