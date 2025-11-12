6de454bab87ae69a3fec9a394bfb81c01474cad7
Jakub Abram <j.abram1@wp.pl>
2025-11-12 14:55:13 +0100

    Added loading component to the blogs page

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -2,22 +2,26 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import api from "../api/axios-setup"
 import Button from "../components/Button.jsx"
+import {LoaderCircle} from "lucide-react"
 
-export function Blogs (){
+export function Blogs() {
 
     const navigate = useNavigate();
     const [blogs, setBlogs] = useState([])
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const getBlogs = async () => {
-            try{
+            try {
                 const response = await api.get("/blogs");
                 setBlogs(response.data);
-            }catch(err){
+            } catch (err) {
                 console.log(err)
                 console.log(response)
                 setError("We failed to fetch blogs from the database.")
+            } finally {
+                setLoading(false)
             }
         }
 
@@ -28,30 +32,37 @@ export function Blogs (){
         <h1 className="w-full text-center text-3xl text-black font-bold py-4">
             View all blogs.
         </h1>
-        <div className="w-full flex flex-col gap-4 lg:grid lg:grid-cols-2 lg:gap-4">
-            {
-                error 
-                    ? <p className="py-2 text-red-600 font-semibold">
-                        {error}
-                    </p> 
-                    : blogs.size == 0
-                        ? <p className="py-2 font-semibold w-full text-center text-sm">
-                            No blogs to display
-                        </p>
-                        : blogs.map((blog, index) => (
-                            <div className="w-full border-2 border-black/10 p-4 rounded-lg" key={index}>
-                                <h1 className="text-nowrap w-full overflow-hidden text-2xl font-semibold text-black pb-4">
-                                    {blog.title}
-                                </h1>
-                                <p className="text-[1.25rem] font-medium text-left">
-                                    {blog.content}
+        {
+            loading
+                ? <div className="p-4 flex items-center justify-center">
+                    <LoaderCircle className="w-6 h-6 animate-spin text-blue-900"/>
+                </div>
+                : <div className="w-full flex flex-col gap-4 lg:grid lg:grid-cols-2 lg:gap-4">
+                    {
+                        error
+                            ? <p className="py-2 text-red-600 font-semibold">
+                                {error}
+                            </p>
+                            : blogs.size == 0
+                                ? <p className="py-2 font-semibold w-full text-center text-sm">
+                                    No blogs to display
                                 </p>
-                                <Button onClick={() => navigate(`/${blog.id}`)}>
-                                    View blog
-                                </Button>
-                            </div>
-                        ))
-            }
-        </div>
+                                : blogs.map((blog, index) => (
+                                    <div className="w-full border-2 border-black/10 p-4 rounded-lg" key={index}>
+                                        <h1 className="text-nowrap w-full overflow-hidden text-2xl font-semibold text-black pb-4">
+                                            {blog.title}
+                                        </h1>
+                                        <p className="text-[1.25rem] font-medium text-left">
+                                            {blog.content}
+                                        </p>
+                                        <Button onClick={() => navigate(`/${blog.id}`)}>
+                                            View blog
+                                        </Button>
+                                    </div>
+                                ))
+                    }
+                </div>
+        }
+
     </div>
 }
