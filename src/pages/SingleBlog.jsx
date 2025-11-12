f206547b7c62f2c470b0b67b2b0f7981dd28f144
Jakub Abram <j.abram1@wp.pl>
2025-11-12 14:46:54 +0100

    Added JSX to single blog page

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -5,6 +5,8 @@ import api from "../api/axios-setup"
 
 export function SingleBlog(){
     
+    const navigate = useNavigate();
+
     const {id: blog_id} = useParams();
     const [blog, setBlog] = useState(null);
     const [error, setError] = useState("")
@@ -23,7 +25,29 @@ export function SingleBlog(){
         getBlogById();
     }, [])
 
-    return <h1>
-        Single Blog page
-    </h1>
+    return <div className="w-full p-4">
+        {
+            error 
+                ? <p className="text-lg text-red-600 font-semibold text-center w-full"> 
+                    {error}
+                </p>
+                : !blog 
+                    ? <p className="text-lg text-center font-semibold">
+                        This blog doesn't exist. <Link to="/">Go home.</Link>
+                    </p> 
+                    : <>
+                        <div className="w-full pt-2 pb-4">
+                            <Button onClick={() => navigate(-1)}>
+                                Go back
+                            </Button>
+                        </div>
+                        <h1 className="text-2xl font-bold text-left py-4">
+                            {blog.title}
+                        </h1>
+                        <p className="text-xl text-left py-2">
+                            {blog.content}
+                        </p>
+                    </>
+        }
+    </div>
 }
