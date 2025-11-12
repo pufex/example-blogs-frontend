66a5e97b9545714c1ffd3b18a3a0b70929308bb7
Jakub Abram <j.abram1@wp.pl>
2025-11-12 14:15:30 +0100

    Added GET blogs request from / (blogs) page in useEffect, and setup states for blogs and error

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react"
+import api from "../api/axios-setup"
+
 export function Blogs (){
 
-    
+    const [blogs, setBlogs] = useState([])
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        const getBlogs = async () => {
+            try{
+                const response = await api.get("/blogs");
+                setBlogs(response.data);
+            }catch(err){
+                console.log(err)
+                console.log(response)
+                setError("We failed to fetch blogs from the database.")
+            }
+        }
+
+        getBlogs();
+    }, [])
 
     return <h1>
         All Blogs page
