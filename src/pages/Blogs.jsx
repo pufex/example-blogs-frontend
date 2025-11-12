9c87489f0e97784d1ab356ab0c94dbf90dde42bc
Jakub Abram <j.abram1@wp.pl>
2025-11-12 14:26:28 +0100

    Added blogs JSX structure

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import api from "../api/axios-setup"
+import Button from "../components/Button.jsx"
 
 export function Blogs (){
 
+    const navigate = useNavigate();
     const [blogs, setBlogs] = useState([])
     const [error, setError] = useState("");
 
@@ -21,7 +24,34 @@ export function Blogs (){
         getBlogs();
     }, [])
 
-    return <div>
-        
+    return <div className="w-full">
+        <h1 className="w-full text-center text-3xl text-black font-bold py-4">
+            View all blogs.
+        </h1>
+        <div className="w-full flex flex-col gap-4 lg:grid lg:grid-cols-2 lg:gap-4">
+            {
+                error 
+                    ? <p className="py-2 text-red-600 font-semibold">
+                        {error}
+                    </p> 
+                    : blogs.size == 0
+                        ? <p className="py-2 font-semibold w-full text-center text-sm">
+                            No blogs to display
+                        </p>
+                        : blogs.map((blog, index) => (
+                            <div className="w-full border-2 border-black/10 p-4 rounded-lg" key={index}>
+                                <h1 className="text-nowrap w-full overflow-hidden text-2xl font-semibold text-black pb-4">
+                                    {blog.title}
+                                </h1>
+                                <p className="text-[1.25rem] font-medium text-left">
+                                    {blog.content}
+                                </p>
+                                <Button onClick={() => navigate(`/${blog.id}`)}>
+                                    View blog
+                                </Button>
+                            </div>
+                        ))
+            }
+        </div>
     </div>
 }
