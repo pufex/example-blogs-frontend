9ba89c25f0de63917f49da09883c007433704f97
Jakub Abram <j.abram1@wp.pl>
2025-11-12 14:31:21 +0100

    Added Button.jsx to components

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+export default function Button({
+    children,
+    onClick
+}) {
+    return <button onClick={onClick} className="px-2 py-1 font-semibold text-center flex items-center gap-2 rounded-lg border-2 border-blue-900/20 bg-blue-600 text-white cursor-pointer transform hover:scale-105 hover:bg-blue-500 active:bg-blue-400 active:scale-95">
+        {children}
+    </button>
+}
