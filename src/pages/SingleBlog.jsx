9f37325b00054f7e52277dfb04ce65356804c0c0
Jakub Abram <j.abram1@wp.pl>
2025-11-12 14:40:43 +0100

    Added GET single post by id to /:id (single blog) page

--- src/pages/SingleBlog.jsx
+++ src/pages/SingleBlog.jsx
@@ -1,4 +1,28 @@
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import api from "../api/axios-setup"
+
+
 export function SingleBlog(){
+    
+    const {id: blog_id} = useParams();
+    const [blog, setBlog] = useState(null);
+    const [error, setError] = useState("")
+
+    useEffect(() => {
+        const getBlogById = async () => {
+            try{
+                const response = api.get(`/blogs/${blog_id}`)
+                setBlog(response.data)
+            }catch(err) {
+                console.log(err)
+                setError("Failed to fetch this blog.")
+            }
+        }
+
+        getBlogById();
+    }, [])
+
     return <h1>
         Single Blog page
     </h1>
