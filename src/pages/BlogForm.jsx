38d58bd78882f76cd6e2bcd184f3e4ab135f5f2c
Jakub Abram <j.abram1@wp.pl>
2025-11-12 18:37:22 +0100

    Added @hookform/devtools package as a dev dependency to the project

--- src/pages/BlogForm.jsx
+++ src/pages/BlogForm.jsx
@@ -1,30 +1,33 @@
 import { useState } from "react"
 import Button from "../components/Button"
+import { useForm } from "react-hook-form"
 import { LoaderCircle } from "lucide-react"
 import api from "../api/axios-setup"
 
-export function BlogForm(){
-    
+export function BlogForm() {
+
+    const { register } = useForm();
+
     const [title, setTitle] = useState("")
     const [content, setContent] = useState("")
 
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
-    
-    const handleSubmit = async(e) => {
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        try{
+        try {
             setLoading(true)
             setError("");
 
-            await api.post("/blogs", {title, content});
+            await api.post("/blogs", { title, content });
 
             setTitle("")
             setContent("")
-        }catch(err){
+        } catch (err) {
             console.log(err)
             setError("Failed to submit the form.")
-        }finally{
+        } finally {
             console.log(error)
             setLoading(false)
         }
@@ -39,19 +42,50 @@ export function BlogForm(){
                 {error}
             </p>
         }
-        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} className="w-full h-10 pl-2 border-2 border-black/30 focus:border-blue-600 rounded-md outline-0 mb-4" placeholder="Your title goes here..." required/>
-        <textarea value={content} onChange={(e) => setContent(e.target.value)} className="w-full border-2 rounded-md border-black/30 focus:border-blue-600 outline-0 mb-4 min-h-30 p-2" required/>
+
+        <div className="flex flex-col gap-2 w-full mb-4">
+            <label
+                htmlFor="title"
+                className="w-full text-lg font-medium text-black"
+            >
+                Title
+            </label>
+            <input
+                type="text"
+                id="title"
+                className="w-full h-10 pl-2 border-2 border-black/30 focus:border-blue-600 rounded-md outline-0"
+                placeholder="Your title goes here..."
+                required
+                {...register("title")}
+            />
+        </div>
+
+        <div className="flex flex-col gap-2 w-full mb-4">
+            <label
+                htmlFor="content"
+                className="w-full text-lg font-medium text-black">
+                Content
+            </label>
+            <textarea
+                id="content"
+                className="w-full border-2 rounded-md border-black/30 focus:border-blue-600 outline-0 mb-4 min-h-30 p-2"
+                required
+                placeholder="What's on your mind?"
+                {...register("content")}
+            />
+        </div>
+
         <div className="w-full">
             {
-                !loading 
+                !loading
                     ? <Button type="submit">
                         Submit
                     </Button>
                     : <div>
-                        <LoaderCircle className="w-6 h-6 animate-spin text-blue-600"/>
+                        <LoaderCircle className="w-6 h-6 animate-spin text-blue-600" />
                     </div>
             }
-            
+
         </div>
     </form>
 }
