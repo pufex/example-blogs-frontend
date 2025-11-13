0cf55d3ca9c99679314f95fa309fcf7e55a1b30b
Jakub Abram <j.abram1@wp.pl>
2025-11-13 18:43:55 +0100

    Added links to login and register pages in the navbar

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -4,8 +4,8 @@ import { Link, Outlet } from "react-router-dom"
 export function Navbar() {
     return <>
         <div className="w-full h-20"/>
-        <nav className="w-full h-20 bg-blue-600 border-b-2 border-blue-800 fixed top-0 left-0">
-            <ul className="w-full mx-auto max-w-7xl px-4 py-2 h-full flex items-center space-x-6">
+        <nav className="w-full h-20 bg-blue-600 border-b-2 border-blue-800 fixed top-0 left-0 flex justify-between">
+            <ul className="mx-auto max-w-7xl px-4 py-2 h-full flex items-center space-x-6">
                 <li>
                     <Link to="/" className="block text-xl text-white font-semibold transform hover:bg-blue-500 px-2 py-1 rounded-lg active:bg-blue-400">
                         Home
@@ -14,6 +14,18 @@ export function Navbar() {
                 <li>
                     <Link to="/create-blog" className="block text-xl text-white font-semibold transform hover:bg-blue-500 px-2 py-1 rounded-lg active:bg-blue-400">
                         New blog
+                    </Link>
+                </li>
+            </ul>
+            <ul className="mx-auto max-w-7xl px-4 py-2 h-full flex items-center space-x-6">
+                <li>
+                    <Link to="/auth/login" className="block text-xl text-white font-semibold transform hover:bg-blue-500 px-2 py-1 rounded-lg active:bg-blue-400">
+                        Login
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/auth/register" className="block text-xl text-white font-semibold transform hover:bg-blue-500 px-2 py-1 rounded-lg active:bg-blue-400">
+                        Register
                     </Link>
                 </li>
             </ul>
