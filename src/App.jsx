8ca1b5ec230f6ff41696b9258305869191b185d5
Jakub Abram <j.abram1@wp.pl>
2025-11-13 18:42:13 +0100

    Added routing to login and register pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Blogs } from "./pages/Blogs"
 import { BlogForm } from "./pages/BlogForm"
 import { SingleBlog } from "./pages/SingleBlog"
 import { Navbar } from "./layouts/Navbar"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 function App() {
   return <>
@@ -11,6 +13,10 @@ function App() {
         <Route index element={<Blogs />} />
         <Route path="create-blog" element={<BlogForm />}/>
         <Route path=":id" element={<SingleBlog />}/>
+        <Route path="/auth">
+          <Route path="login" element={<Login />}/>
+          <Route path="register" element={<Register />}/>
+        </Route>
       </Route>
     </Routes>
   </>
