552859ea422af75fb5f37d2ba8e130a7e8da9a76
Jakub Abram <j.abram1@wp.pl>
2025-11-13 18:48:55 +0100

    Added className prop to the Button.jsx component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,12 @@ export default function Button({
     children,
     onClick,
     type = "button",
-    disabled = false
+    disabled = false,
+    className = ""
 }) {
     return <button
         onClick={onClick}
-        className={`px-8 h-10 font-semibold text-center flex items-center gap-2 rounded-lg border-2 border-blue-900/20 text-white cursor-pointer transform bg-blue-600 hover:scale-105 hover:bg-blue-500 active:bg-blue-400 active:scale-95 `}
+        className={`px-8 h-10 font-semibold text-center flex items-center gap-2 rounded-lg border-2 border-blue-900/20 text-white cursor-pointer transform bg-blue-600 hover:scale-105 hover:bg-blue-500 active:bg-blue-400 active:scale-95 ${className}`}
         type={type}
         disabled={disabled}
     >
