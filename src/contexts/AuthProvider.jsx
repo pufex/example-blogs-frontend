f16f78eca40dac0c41312aeb5d48faa99ab3b918
Jakub Abram <j.abram1@wp.pl>
2025-11-14 10:43:50 +0100

    Added Auth Context with Auth Provider and useAuth hook

---
+++ src/contexts/AuthProvider.jsx
@@ -0,0 +1,15 @@
+import { createContext, useState, useContext } from "react";
+
+const AuthContext = createContext({})
+
+export const useAuth = () => {
+    return useContext(AuthContext)
+}
+
+export const AuthProvider = ({children}) => {
+    const [auth, setAuth] = useState(null)
+
+    return <AuthContext.Provider value={{auth, setAuth}}>
+        {children}
+    </AuthContext.Provider>
+}
