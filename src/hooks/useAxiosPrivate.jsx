e400c7ec48cd6098bdefafeb038cc5f7b1784fe4
Jakub Abram <j.abram1@wp.pl>
2025-11-15 10:38:08 +0100

    Created private axios instance and added interceptors to both include access token and retry axios calls once after a 403 status code

---
+++ src/hooks/useAxiosPrivate.jsx
@@ -0,0 +1,42 @@
+import { useAuth } from "../contexts/AuthProvider"
+import { useEffect } from "react"
+import { axiosPrivate } from "../api/axios-setup"
+
+export const  useAxiosPrivate = () => {
+    const {refresh, auth} = useAuth();
+
+    useEffect(() => {
+
+        const requestIntercept = axiosPrivate.interceptors.request.use(
+            config => {
+                if(!config.headers["authorization"]){
+                    config.headers["authorization"] = `Bearer ${auth.accessToken}`
+                }
+
+                return config
+            },
+            err => Promise.reject(err)
+        )
+
+        const responseIntercept = axiosPrivate.interceptors.response.use(
+            config => config,
+            async (err) => {
+                const prevRequest = err?.config
+                if(error?.response.status === 403 && !prevRequest?.sent){
+                    prevRequest.sent = true
+                    const newAccessToken = await refresh();
+                    prevRequest.headers['authorization'] = `Bearer ${newAccessToken}`
+                    return axiosPrivate(prevRequest)
+                }
+                return Promise.reject(err)
+            }
+        )
+
+        return () => {
+            axiosPrivate.interceptors.response.eject(responseIntercept)
+            axiosPrivate.interceptors.request.eject(requestIntercept)
+        }
+    }, [auth, refresh])
+
+    return axiosPrivate;    
+} 
--- src/hooks/useRefreshToken.jsx
+++
@@ -1,5 +0,0 @@
-import {useAuth} from "../contexts/AuthProvider"
-
-export const useRefreshToken = () => {
-    
-}
